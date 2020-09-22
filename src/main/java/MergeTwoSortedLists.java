e6fe949ee5b52ed69384aeab1799d75d6117f910
hao <hao.deng.m@gmail.com>
2020-09-22 22:53:52 +0200

    Optimize MergeTwoSortedLists

--- src/main/java/MergeTwoSortedLists.java
+++ src/main/java/MergeTwoSortedLists.java
@@ -13,13 +13,6 @@ public class MergeTwoSortedLists {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
 
-        //Be sure the 1st node of L1 is always < the 1st node of L2
-        if (l1.val > l2.val) {
-            ListNode tempNode = l2;
-            l2 = l1;
-            l1 = tempNode;
-        }
-
         ListNode newList = null;
         while (l1 != null && l2 != null) {
             if (l2.val <= l1.val) {
